d5ef3b8a4f087d81193f3a56fc6762a085c9c3ae
SaraMouatadid <sara.mouatadid@gmail.com>
2023-03-16 15:07:20 -0700

    first c code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * print Programming is like building a multilingual puzzle
+ * to the stdout
+ *
+ * Return Always 0 (success)
+ */
+
+int  main(void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
