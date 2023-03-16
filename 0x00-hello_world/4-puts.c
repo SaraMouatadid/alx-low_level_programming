5a9a8c2956f94685cdfd613d84faaca8c8b192e7
SaraMouatadid <sara.mouatadid@gmail.com>
2023-03-16 15:18:28 -0700

    first c code fixing betty style issues

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,11 +6,11 @@
  * print Programming is like building a multilingual puzzle
  * to the stdout
  *
- * Return Always 0 (success)
+ * Return: Always 0 (success)
  */
 
-int  main(void)
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
