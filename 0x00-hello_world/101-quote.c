f108a5eb2a430ca7e3023b8a20350182df4d9675
SaraMouatadid <sara.mouatadid@gmail.com>
2023-03-16 17:02:11 -0700

    printing error message

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * sends msg to the Standard Error
+ *
+ * Return: Always 0 (succes)
+ */
+
+int main(void)
+{
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
