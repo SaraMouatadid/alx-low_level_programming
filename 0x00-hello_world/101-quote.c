51c933652ee62d7ba69146ba0fd61fd788daf7f3
SaraMouatadid <sara.mouatadid@gmail.com>
2023-03-16 17:23:17 -0700

    sending an error msg to stderr quick fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  *
  * sends msg to the Standard Error
  *
- * Return: Always 0 (succes)
+ * Return: Always 1 (succes)
  */
 
 int main(void)
 {
-	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
