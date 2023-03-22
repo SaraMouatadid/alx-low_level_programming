6d8c85ff55777aef33e3b56fadcb3ce30584cc41
SaraMouatadid <sara.mouatadid@gmail.com>
2023-03-21 17:10:29 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an input number
+ *
+ * @n: the unput number
+ *
+ * Return: the absolute value of n
+ */
+int _abs(int n)
+{
+	return (n >= 0 ? n : -1 * n);
+}
