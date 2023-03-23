8a03c0dc68c28b12e38b7f0d0b6eaf7cb10db9ab
SaraMouatadid <sara.mouatadid@gmail.com>
2023-03-23 13:52:08 -0700

    commit the mul function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiplies two input numbers
+ *
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
