fb8dcc7f4d4565dd47e476a526dcbc7a7cef8ee7
SaraMouatadid <sara.mouatadid@gmail.com>
2023-03-30 18:40:04 -0700

    fix code for task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,22 +1,26 @@
 #include "main.h"
 
 /**
- * print_number - prints an input integer using _putchar
- *
- * @n: the input integer
- *
- * Return: no return statement
+ * print_number - print numbers chars
+ * @n: integer params
+ * Return: 0
  */
+
 void print_number(int n)
 {
-	int n1 = n;
+	unsigned int n1;
+
+	n1 = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
 		n1 = -n;
 	}
+
 	if (n1 / 10 != 0)
+	{
 		print_number(n1 / 10);
+	}
 	_putchar((n1 % 10) + '0');
 }
